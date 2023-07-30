1991112889fc924ee2b039b80eb0184e3e614a12
Kay Eugenia Purnama <122983473+kayeugenia@users.noreply.github.com>
2023-07-30 19:48:08 +0800

    user seed

--- task-5-fullstack/app/Http/Controllers/PostController.php
+++ task-5-fullstack/app/Http/Controllers/PostController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Article;
 
 class PostController extends Controller
 {
     //
+    public function index()
+    {
+        $articles = Article::with('category')->paginate(10); 
+        return response()->json($articles);
+    }
 }
---
+++ task-5-fullstack/database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'password' => bcrypt('password123'),
+        ]);
+
+        User::create([
+            'name' => 'Jane Smith',
+            'email' => 'jane@example.com',
+            'password' => bcrypt('secret321'),
+        ]);
+        //
+    }
+}
